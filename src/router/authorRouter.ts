69766584b25f8c8b1fdea43c97e915e489ecb9c1
Nguyen Cong Duy <ncongduy@gmail.com>
2022-03-11 19:19:23 +0700

    feat: create CRUD for authorRouter

--- src/router/authorRouter.ts
+++ src/router/authorRouter.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const authorRouter = express.Router();
 
 authorRouter.get('/', (req, res) => {
-	res.send('author router');
+	res.send('get author information');
+});
+
+authorRouter.post('/', (req, res) => {
+	res.send('create author');
+});
+
+authorRouter.put('/', (req, res) => {
+	res.send('update author');
+});
+
+authorRouter.delete('/', (req, res) => {
+	res.send('delete author');
 });
 
 export { authorRouter };
