493e1b65caaf00a002aece98e189b4f1c3003bb6
Nguyen Cong Duy <ncongduy@gmail.com>
2022-03-11 19:19:03 +0700

    feat: create CRUD for userRouter

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const userRouter = express.Router();
 
 userRouter.get('/', (req, res) => {
-	res.send('user router');
+	res.send('get user information');
+});
+
+userRouter.post('/', (req, res) => {
+	res.send('create user');
+});
+
+userRouter.put('/', (req, res) => {
+	res.send('update user');
+});
+
+userRouter.delete('/', (req, res) => {
+	res.send('delete user');
 });
 
 export { userRouter };
