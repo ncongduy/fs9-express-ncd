c33d850cdfd283a8b67f596692bcd7210b0fcf50
Nguyen Cong Duy <ncongduy@gmail.com>
2022-03-11 19:19:58 +0700

    feat: create CRUD for bookRouter

--- src/router/bookRouter.ts
+++ src/router/bookRouter.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const bookRouter = express.Router();
 
 bookRouter.get('/', (req, res) => {
-	res.send('book router');
+	res.send('get book information');
+});
+
+bookRouter.post('/', (req, res) => {
+	res.send('create book');
+});
+
+bookRouter.put('/', (req, res) => {
+	res.send('update book');
+});
+
+bookRouter.delete('/', (req, res) => {
+	res.send('delete book');
 });
 
 export { bookRouter };
